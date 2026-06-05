b09880d62e60c0c1da42f31d2cd577cc6debef0c
matti <mattiaedoardo.blasio01@universitadipavia.it>
2026-06-05 08:48:29 +0200

    added InterfacciaAmministratoreDAO.java to DAO.interface

---
+++ src/it/unipv/JVL_DA/project/DAO/inteface/InterfacciaAmministratoreDAO.java
@@ -0,0 +1,25 @@
+package it.unipv.JVL_DA.project.DAO.inteface;
+
+import it.unipv.JVL_DA.project.POJO.Amministratore;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface InterfacciaAmministratoreDAO {
+    boolean insert(Amministratore amministratore) throws SQLException;
+
+    Amministratore cercaID(int id) throws SQLException;
+
+    Amministratore cercaAdmin_user(String adminUser);
+
+    Amministratore cercaEmail(String email) throws SQLException;
+
+    List<Amministratore> cercaTutti() throws SQLException;
+
+    boolean update(Amministratore amministratore) throws SQLException;
+
+    boolean delete(int id) throws SQLException;
+
+    Amministratore login(String adminUser, String plainPassword) throws SQLException;
+}
+
